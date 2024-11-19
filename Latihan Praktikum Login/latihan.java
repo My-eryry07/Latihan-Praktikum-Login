8979fdba7421a7fff9078a37ad0a064abe209613
Rizky Yuli Andreanto <110170111+My-eryry07@users.noreply.github.com>
2024-11-19 19:46:33 +0700

    Add files via upload

---
+++ Latihan Praktikum Login/latihan.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Latihan;
+
+/**
+ *
+ * @author user
+ */
+public class latihan {
+    public static void main(String[] args) {
+        new menuUtama().setVisible(true);
+    }
+}
